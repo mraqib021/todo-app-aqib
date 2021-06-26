794c928c631801e125baf5001f4f5199ce786304
aqib <mraqib021@gmail.com>
2021-06-26 22:25:25 +0500

    New features Add

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
-var a = document.getElementById("list");
-var x = document.getElementById("input");
-var flag = false;
-
-function add(){
-    var li = document.createElement("li")
-    a.appendChild(li)
-    li.setAttribute("style", "list-style-type:none")
-    li.innerHTML = x.value
-    x.value = ""
-    var edit = document.createElement("button")
-    var dlt = document.createElement("button")
-    edit.innerHTML = "Edit"
-    dlt.innerHTML = "Delete"
-    li.appendChild(edit);
-    li.appendChild(dlt); 
+var task = document.getElementById("textBox");
+task.setAttribute('Placeholder', 'Enter task to add in the list');
+function taskAdd() {
+    if (task.value == "  " || task.value == "" || task.value == " ") {
+        task.setAttribute('Placeholder', 'You dont have anything to add in the list');
+    }
+    else {
+        var list = document.createElement("li");
+        var listText = document.createElement("input");
+        listText.setAttribute('disabled', 'disabled');
+        listText.setAttribute('class','inputBox')
+        listText.value = task.value.toUpperCase();
+        var editBtn = document.createElement('button')
+        var edittext = document.createTextNode('Edit')
+        editBtn.setAttribute('class','edit')
+        editBtn.appendChild(edittext)
+        editBtn.setAttribute('onclick', 'edit(this.previousSibling)')
+        var delBtn = document.createElement('button')
+        var deltext = document.createTextNode('Delete')
+        delBtn.setAttribute('class','del')
+        delBtn.appendChild(deltext);
+        delBtn.setAttribute('onclick', 'del(this)')
+        delBtn.setAttribute('id', 'delete')
+        list.appendChild(listText);
+        list.appendChild(editBtn);
+        list.appendChild(delBtn);
+        listMain.appendChild(list);
+        task.setAttribute('Placeholder', 'Enter task to add in the list');
+        task.value = "";
+    }
 }
-function dlte(){
-    a.remove();
-    flag = true;
+// To Edit the list
+function edit(listData) {
+    var del = document.getElementById("delete");
+    var check = listData.nextSibling.innerText;
+    if (check === "Edit") {
+        listData.disabled = false;
+        listData.focus();
+        listData.nextSibling.innerText = "Update";
+        del.disabled = true;
+    }
+    else if (check === "Update") {
+        listData.disabled = true;
+        listData.nextSibling.innerText = "Edit";
+        del.disabled = false;
+    }
+}
+// To Delete the list item
+function del(removeItem) {
+    removeItem.parentNode.remove();
 }
